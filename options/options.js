6e6d2707ce86f7b19ee576400b7e7f9bf4008d6a
Crowdbotics <team@crowdbotics.com>
2024-03-22 07:43:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "petsitting-ai-prd-p-47209",
+  url: "https://petsitting-ai-prd-p-47209.botics.co",
+  api: "https://petsitting-ai-prd-p-47209.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
